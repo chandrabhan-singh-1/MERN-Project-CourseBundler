b0be2ff65f095a68774fff068952b3a803f191f4
chandrabhan-singh-1 <chandrabhansingh813@gmail.com>
2023-09-11 19:14:56 +0530

    Updated cors

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: true,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
